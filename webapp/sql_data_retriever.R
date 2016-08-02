898275c500a46ac78e20013a3937d6ad0ac27011
DarronTang <tangdarron@gmail.com>
2016-08-02 17:07:59 +0100

    Function which retrieves a dataset from a specified table in the database either all/specified course and all/specified run

--- webapp/sql_data_retriever.R
+++ webapp/sql_data_retriever.R
@@ -1,5 +1,25 @@
-import mysql.connector
+require(RMySQL)
 
-getTable <- function(table,university,course,run){
-	
+getTable <- function(table,course,course_run){
+	m<-dbDriver("MySQL");
+	con<-dbConnect(m,user='root',password='moocDashboard1',host='localhost',dbname='moocs');
+
+	table <- paste0(c(table," t "))
+	where <- ""
+	course1 <- "t.course = "
+	course_run1 <- "t.course_run = "
+
+	if(course != "All" && course_run != "All"){
+		where <- paste0("WHERE ", course1,course,",",course_run1,courserun)
+	} else if(course != "All"){
+		where <- paste0("WHERE ", course1,course)
+	} else if(course_run != "All"){
+		where <- paste0("WHERE ", course_run1,course_run)
+	}
+
+	query <- paste0(c("SELECT * FROM ",table,where))
+	sendQuery<-dbSendQuery(con, query)
+	data<- fetch(sendQuery, n = -1)
+	dbDisconnect(con)
+	return(data)
 }
