25c649ed2549fcf5e192ffc24cb33ad792aae4a6
Mooc Observatory <mooc@rsg-xen-vm03.ecs.soton.ac.uk>
2016-10-19 17:10:25 +0100

    Fixed the edge case of a course with 0 runs

--- update/courses_run.py
+++ update/courses_run.py
@@ -38,43 +38,46 @@ class FLCourses:
 
 			for table in tables:
 				for course in table.find_all('tbody'):
-					course_name = course.a['title']
-					print "Found course: %s ..." %course_name
-					#get courses run in different time
-					course_runs = course.find_all('tr')
-					run_count = len(course_runs)
-					print "...with %d runs" %run_count
-					course_info = {}
+					try:
+						course_name = course.a['title']
+						print "Found course: %s ..." %course_name
+						#get courses run in different time
+						course_runs = course.find_all('tr')
+						run_count = len(course_runs)
+						print "...with %d runs" %run_count
+						course_info = {}
 
-					for course_run in course_runs:
-						l = course_run.find_all('span')
-						_start_date = l[0].text
-						print "...start date: %s " % _start_date
-						_status = l[1].text.lower()
-						print "...status: %s " % _status
-						_stats_path = course_run.find(title = 'View stats')['href']
-						_run_details_path = course_run.find(title = 'Run details')['href']
+						for course_run in course_runs:
+							l = course_run.find_all('span')
+							_start_date = l[0].text
+							print "...start date: %s " % _start_date
+							_status = l[1].text.lower()
+							print "...status: %s " % _status
+							_stats_path = course_run.find(title = 'View stats')['href']
+							_run_details_path = course_run.find(title = 'Run details')['href']
 
-						# Fetch data of finished courses only
-						if( _status == 'finished' or _status == 'in progress' ):
+							# Fetch data of finished courses only
+							if( _status == 'finished' or _status == 'in progress' ):
 
-							run_duration_weeks = self.getRunDuration(self.__mainsite + _run_details_path)
+								run_duration_weeks = self.getRunDuration(self.__mainsite + _run_details_path)
 
-							# Convert to Date type and compute end date
-							# Pad if needed. e.g. 9 May 2016 to 09 May 2016
-							if(len(_start_date) == 10):
-								_start_date = "0"+_start_date
+								# Convert to Date type and compute end date
+								# Pad if needed. e.g. 9 May 2016 to 09 May 2016
+								if(len(_start_date) == 10):
+									_start_date = "0"+_start_date
 
-							start_date = datetime.datetime.strptime(_start_date, "%d %b %Y")
-							end_date = start_date + datetime.timedelta(weeks=int(run_duration_weeks))
-							print "...end date: %s" %end_date
+								start_date = datetime.datetime.strptime(_start_date, "%d %b %Y")
+								end_date = start_date + datetime.timedelta(weeks=int(run_duration_weeks))
+								print "...end date: %s" %end_date
 
-							run_data = {'start_date': start_date , 'end_date': end_date, 'duration_weeks' : run_duration_weeks, 'status' : _status, 'datasets' : self.getDatasets(self.__mainsite + _stats_path), 'enrolmentData' : self.getEnrolmentData(self.__mainsite + _stats_path + "/overview")}
-							course_info[str(run_count)] = run_data
-					
-						run_count-=1
+								run_data = {'start_date': start_date , 'end_date': end_date, 'duration_weeks' : run_duration_weeks, 'status' : _status, 'datasets' : self.getDatasets(self.__mainsite + _stats_path), 'enrolmentData' : self.getEnrolmentData(self.__mainsite + _stats_path + "/overview")}
+								course_info[str(run_count)] = run_data
+						
+							run_count-=1
 
-					courses[course_name] = course_info
+						courses[course_name] = course_info
+					except:
+						print "Course was in an invalid format. HTML: {!r}".format(course)
 		
 			return courses
 		
